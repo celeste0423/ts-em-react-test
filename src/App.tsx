4fb402551cb2fc11c218aaed71f33a753452a547
celeste0423 <jyeob25@korea.ac.kr>
2023-09-18 11:07:55 +0900

    reactive ui homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,94 @@
 import React from "react";
 import HomePage from "./pages/Home.page";
+import { Global, css } from "@emotion/react";
+
+const globalStyle = css`
+  @import url("https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;700&family=Poppins:wght@300;400;500;600;700&display=swap");
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Cabin", sans-serif;
+  }
+
+  :root {
+    --primaryColor: #256af4;
+    --secondaryColor: #729ffa;
+    --backgroundLightGreyColor: #f9fafc;
+    --whiteColor: #ffffff;
+    --blackColor: #000000;
+    --blackCardBackgroundColor: #19191980;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  h1 {
+    font-size: 45px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  .section {
+    padding: 4rem 0 2rem;
+  }
+
+  .container {
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
+  }
+
+  .icon {
+    font-size: 2rem;
+    cursor: pointer;
+  }
+
+  .flex {
+    display: flex;
+    align-items: center;
+  }
+
+  .grid {
+    display: grid;
+    align-items: center;
+  }
+
+  .btn {
+    padding: 0.6rem 1.5rem;
+    color: var(--whiteColor);
+    background: var(--primaryColor);
+    border-radius: 0.5rem;
+    border: none;
+    outline: none;
+    cursor: pointer;
+
+    &:hover {
+      background: var(--secondaryColor);
+    }
+  }
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  body {
+    background: var(--backgroundLightGreyColor);
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <Global styles={globalStyle} />
       <HomePage />
     </div>
   );
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,41 +1,119 @@
 import React from "react";
-import Header from "../components/Header";
+import { css } from "@emotion/react";
 
-import { Button, Code, Text, Title } from "@mantine/core";
-import HeaderAction from "../components/Header";
+import brainDesktopImage from "../assets/brain-desktop.svg";
 
-const linksData = [
-  {
-    link: "/home",
-    label: "Home",
-    links: [
-      { link: "/about", label: "About" },
-      { link: "/contact", label: "Contact" },
-    ],
-  },
-  {
-    link: "/products",
-    label: "Products",
-    links: [
-      { link: "/product1", label: "Product 1" },
-      { link: "/product2", label: "Product 2" },
-    ],
-  },
-];
+const homePageStyle = css`
+  :root {
+    --primaryColor: #256af4;
+    --secondaryColor: #729ffa;
+    --backgroundLightGreyColor: #f9fafc;
+    --whiteColor: #ffffff;
+    --blackColor: #000000;
+    --greyColor: #454545;
+    --blackCardBackgroundColor: #19191980;
+  }
+
+  .contentGrid {
+    grid-template-columns: 50% 50%;
+    place-items: left;
+    padding: 2rem;
+    margin: 1rem;
+
+    .imageDiv {
+      img {
+        height: 600px;
+      }
+    }
+    .textDiv {
+      width: 100%;
+      height: fit-content;
+      align-items: center;
+      flex-direction: column;
+      .title {
+        font-size: 85px;
+      }
+      .discription {
+        padding: 2rem 0 2rem 0;
+      }
+      .buttonDiv {
+        width: 100%;
+        align-items: left;
+        .btn {
+          margin-right: 1rem;
+          height: 60px;
+          width: 170px;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 1125px) {
+    .contentGrid {
+      grid-template-columns: none;
+      place-items: center;
+      padding: 2rem;
+      margin: 1rem;
+
+      .imageDiv {
+        img {
+          height: 600px;
+        }
+      }
+
+      .textDiv {
+        width: 100%;
+        align-items: center;
+        flex-direction: column;
+        .title {
+          text-align: center;
+        }
+        .discription {
+          width: 60%;
+          text-align: center;
+        }
+        .buttonDiv {
+          width: 100%;
+          .btn {
+            width: 100%;
+            height: 70px;
+            margin: 1rem;
+          }
+        }
+      }
+    }
+  }
+
+  .subButton {
+    background: var(--whiteColor);
+    color: var(--blackColor);
+    border: 1px solid var(--greyColor) !important;
+  }
+`;
 
 function HomePage() {
   return (
-    <div>
-      <HeaderAction links={linksData} />
-      <Title order={2}>반가워요</Title>
-      <Text color="dimmed" weight="bold">
-        You're logged in!
-      </Text>
-      <Code block mt={12}>
-        반가워요
-      </Code>
-      <Button>Sign Out</Button>
-    </div>
+    <section css={homePageStyle} className="getStarted">
+      <div className="contentGrid grid">
+        <div className="imageDiv">
+          <img src={brainDesktopImage} alt="" />
+        </div>
+
+        <div className="textDiv flex">
+          <h1 className="title">Learn Medicine the Smart Way</h1>
+
+          <p className="discription">
+            Quesmed is a cutting-edge learning platform designed to help you
+            excel at your exams and succeed as a doctor
+          </p>
+
+          <div className="buttonDiv">
+            <button className="btn">Get started</button>
+            <button className="btn subButton">Try now</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
