9a50eeb70c2de5107936931890841fb5662cb6ac
James Ryu <jamesryu392@gmail.com>
2023-09-27 16:05:03 +0900

    Created QuizNavBar and RadioToggle, also added more svgs in the assets folder

---
+++ src/components/quizPage/RadioToggle.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+interface RadioToggleProps {
+  options: string[];
+  defaultOption?: string;
+}
+
+const RadioToggle: React.FC<RadioToggleProps> = ({ options, defaultOption }) => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(defaultOption || null);
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div>
+      {options.map((option) => (
+        <label key={option} className={`radio-button ${selectedOption === option ? 'active' : ''}`}>
+          <input
+            type="radio"
+            name="toggle"
+            value={option}
+            checked={selectedOption === option}
+            onChange={() => handleOptionChange(option)}
+            style={{ display: 'none'}}
+          />
+          {option}
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export default RadioToggle;
