d858efc03c990b1844ff5f7634c82da5a8c0f43c
celeste0423 <jyeob25@korea.ac.kr>
2023-10-10 17:24:41 +0900

    counting fuction needed

--- src/components/quizPage/ReviewCards.tsx
+++ src/components/quizPage/ReviewCards.tsx
@@ -5,112 +5,6 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { quizProgressAtom } from "../../store/Atom";
 import { useAtom } from "jotai";
 
-type Props = {
-  onPressCard: (index: number) => void;
-};
-
-const reviewCardStyle = css`
-  :root {
-    --primaryColor: #256af4;
-    --secondaryColor: #729ffa;
-    --backgroundLightGreyColor: #f9fafc;
-    --whiteColor: #ffffff;
-    --blackColor: #000000;
-    --greyColor: #454545;
-    --blackCardBackgroundColor: #19191980;
-    --greenColor: #4ad9bc;
-    --lightGreenColor: rgba(74, 217, 188, 0.15);
-    --redColor: #ff5c5c;
-    --lightRedColor: rgba(255, 92, 92, 0.15);
-  }
-
-  height: 100vb;
-
-  .content {
-    display: grid;
-    overflow: visible;
-    position: relative;
-    width: 100%;
-
-    margin-top: 85px;
-
-    background: var(--whiteColor);
-    padding: 1rem 2rem 60% 2rem;
-    border-radius: 0.3rem;
-    box-shadow: -2px 1px 4px rgba(0, 0, 0, 0.101);
-
-    .arrow_button {
-      position: absolute;
-      background: var(--whiteColor);
-      box-shadow: -2px 1px 4px rgba(0, 0, 0, 0.101);
-      border-radius: 1rem;
-      width: 2rem;
-      height: 2rem;
-      top: 3rem;
-      left: -1rem;
-      padding-top: 0.4rem;
-      padding-left: 0.25rem;
-    }
-    @media screen and (min-width: 1125px) {
-      .arrow_button {
-        display: none;
-      }
-    }
-
-    .quizNumber {
-      padding-bottom: 1rem;
-    }
-  }
-  .grid_container {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-row-gap: 18px;
-    grid-column-gap: 9px;
-  }
-
-  .indexButton {
-    background-color: #eef0f2;
-    color: #1f262c;
-    display: inline-block;
-    padding: 10px;
-    border-radius: 5px;
-    border: none;
-    height: 52px;
-    width: 52px;
-    text-align: center;
-    font-weight: bold;
-  }
-  .activeIndexButton {
-    background-color: #d3eaf8;
-    border-radius: 5px;
-    border: 1.5px solid #387fcf;
-  }
-  .rightIndexButton {
-    background: rgba(74, 217, 188, 0.4);
-    color: #1f262c;
-    display: inline-block;
-    padding: 10px;
-    border-radius: 5px;
-    border: none;
-    height: 52px;
-    width: 52px;
-    text-align: center;
-    font-weight: bold;
-  }
-  .wrongIndexButton {
-    background: rgba(255, 92, 92, 0.4);
-    color: #1f262c;
-    display: inline-block;
-    padding: 10px;
-    border-radius: 5px;
-    border: none;
-    height: 52px;
-    width: 52px;
-    text-align: center;
-    font-weight: bold;
-  }
-`;
-
 type reviewCardType = {
   quizIndex: number;
   setQuizIndex: (index: number) => void;
@@ -126,6 +20,132 @@ const ReviewCards: React.FC<reviewCardType> = ({
   setToggleReviewCard,
   quizLength,
 }) => {
+  const reviewCardStyle = css`
+    :root {
+      --primaryColor: #256af4;
+      --secondaryColor: #729ffa;
+      --backgroundLightGreyColor: #f9fafc;
+      --whiteColor: #ffffff;
+      --blackColor: #000000;
+      --greyColor: #454545;
+      --blackCardBackgroundColor: #19191980;
+      --greenColor: #4ad9bc;
+      --lightGreenColor: rgba(74, 217, 188, 0.15);
+      --redColor: #ff5c5c;
+      --lightRedColor: rgba(255, 92, 92, 0.15);
+    }
+
+    height: 100vb;
+
+    .content {
+      display: grid;
+      overflow: visible;
+      position: relative;
+      width: 100%;
+
+      margin-top: 85px;
+
+      background: var(--whiteColor);
+      padding: 1rem 2rem 60% 2rem;
+      border-radius: 0.3rem;
+      box-shadow: -2px 1px 4px rgba(0, 0, 0, 0.101);
+
+      .arrow_button {
+        position: absolute;
+        background: var(--whiteColor);
+        box-shadow: -2px 1px 4px rgba(0, 0, 0, 0.101);
+        border-radius: 1rem;
+        width: 2rem;
+        height: 2rem;
+        top: 3rem;
+        left: -1rem;
+        padding-top: 0.4rem;
+        padding-left: 0.25rem;
+      }
+      @media screen and (min-width: 1125px) {
+        .arrow_button {
+          display: none;
+        }
+      }
+
+      .progressBar {
+        background: #eef0f2;
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        height: 8px;
+        width: 100% - 20px;
+        border-radius: 1rem;
+
+        .progressBarRight {
+          background: #4ad9bc;
+          height: 100%;
+          width: ${20 * 1}px;
+          border-top-left-radius: 1rem;
+          border-bottom-left-radius: 1rem;
+          border-top-right-radius: ${false ? "1rem" : null};
+          border-bottom-right-radius: ${false ? "1rem" : null};
+        }
+        .progressBarWrong {
+          height: 100%;
+          background: #ff5c5c;
+          width: ${20 * 1}px;
+          border-top-right-radius: 1rem;
+          border-bottom-right-radius: 1rem;
+          border-top-left-radius: ${false ? "1rem" : null};
+          border-bottom-left-radius: ${false ? "1rem" : null};
+        }
+      }
+    }
+    .grid_container {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      grid-row-gap: 18px;
+      grid-column-gap: 9px;
+    }
+
+    .indexButton {
+      background-color: #eef0f2;
+      color: #1f262c;
+      display: inline-block;
+      padding: 10px;
+      border-radius: 5px;
+      border: none;
+      height: 52px;
+      width: 52px;
+      text-align: center;
+      font-weight: bold;
+    }
+    .activeIndexButton {
+      background-color: #d3eaf8;
+      border-radius: 5px;
+      border: 1.5px solid #387fcf;
+    }
+    .rightIndexButton {
+      background: rgba(74, 217, 188, 0.4);
+      color: #1f262c;
+      display: inline-block;
+      padding: 10px;
+      border-radius: 5px;
+      border: none;
+      height: 52px;
+      width: 52px;
+      text-align: center;
+      font-weight: bold;
+    }
+    .wrongIndexButton {
+      background: rgba(255, 92, 92, 0.4);
+      color: #1f262c;
+      display: inline-block;
+      padding: 10px;
+      border-radius: 5px;
+      border: none;
+      height: 52px;
+      width: 52px;
+      text-align: center;
+      font-weight: bold;
+    }
+  `;
+
   const [quizProgress, setQuizProgress] = useAtom(quizProgressAtom);
 
   const buttons = Array.from({ length: quizLength }, (_, index) => {
@@ -181,7 +201,10 @@ const ReviewCards: React.FC<reviewCardType> = ({
           Review cards •{quizIndex + 1}/{quizLength}
         </h4>
 
-        <div className="progressBar"></div>
+        <div className="progressBar flex">
+          <div className="progressBarRight" />
+          <div className="progressBarWrong" />
+        </div>
 
         <div className="grid_container">{buttons}</div>
       </div>
